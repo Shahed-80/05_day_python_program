ee8ff8cd1aa7f97b155d161997cea475bbcc74db
Shahed-80 <107167352+Shahed-80@users.noreply.github.com>
2022-06-15 10:42:13 +0530

    Add files via upload

---
+++ 05_day_ways_assign_variables.py
@@ -0,0 +1,47 @@
+''' Ways to assign variables'''
+
+p = 3.6
+q = 89
+r = "str"
+
+print(p, q, r)
+
+print(p)
+print(q)
+print(r)
+
+T = S = 'int'; Q = 'iron'
+print(T,S,Q)
+
+x = "python"
+z = x.upper()
+z = x.lower()
+print(z)
+
+d,r,s = 10,45,85,8.6
+print(d,r,s)
+
+t,x,r,t= 25,56
+print(t,x)
+
+
+
+t = 3.6
+g = 10.6
+t = t+g
+print(t)
+
+
+d = "Python"
+x = " is a program"
+d = d+x
+print(d)
+
+
+
+
+n = 1,2,3,4,5,6
+print(n, type(n))
+
+# n,m = 1,2,3,4,5
+
